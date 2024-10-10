fc5d5a64406c5245a0d1c44abd22ebe6b10c0af8
Mizerael <dan-acer@outlook.com>
2024-10-10 17:51:21 +0400

    Увидел вторую часть 4 задачи

--- src/task1/second.py
+++ src/task1/second.py
@@ -1,11 +1,13 @@
 def task2():
     inp = int(input())
-    negative = 0
+    negative = positive = 0
     start = min(inp, -inp)
     end = max(inp, -inp) + 2  # в задаче указан [-v, v} , возможно это опечатка
     for num in range(start, end):
         if num < 0:
             negative += num
+        else:
+            positive += num
         print(num)
     print(
-        f"Сумма отрицательных: {negative}\n Сумма положительных: {-negative}")
+        f"Сумма отрицательных: {negative}\n Сумма положительных: {positive}")
