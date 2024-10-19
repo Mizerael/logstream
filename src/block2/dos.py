ae000da5bbdee7d7a87c4c2f091103120afc6f67
Mizerael <dan-acer@outlook.com>
2024-10-20 02:14:51 +0400

    Блок2: решение задачи 2

---
+++ src/block2/dos.py
@@ -0,0 +1,11 @@
+def task2() -> None:
+    inp = input().lower().replace(' ', '')
+    chars = {}
+
+    for val in set(inp):
+        chars[val] = inp.count(val)
+    print(chars)
+
+
+if __name__ == "__main__":
+    task2()
