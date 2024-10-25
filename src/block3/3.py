df5b00d78557c29ea609bf8661ac80045510e57b
Mizerael <dan-acer@outlook.com>
2024-10-26 00:39:46 +0400

    Блок3: решение задачи 3

---
+++ src/block3/3.py
@@ -0,0 +1,28 @@
+def function_name(search: str, status: bool,
+                  *args: list | tuple | set, **kwargs: dict) -> list | str:
+    result = []
+    result_2 = ""
+    if search == "args":
+        if status:
+            for i in args:
+                if isinstance(i, int):
+                    result.append(i)
+            return result
+        else:
+            for i in args:
+                result_2 += f" {i}"
+            return result_2
+    elif search == "kwargs":
+        for k, v in kwargs.items():
+            result_2 += (" Key: {}, Value: {}; ".format(k, v))
+        return result_2
+    else:
+        raise ValueError("Error for search")
+
+
+print(function_name("args", True, *[1, "sst", 2]))
+print(function_name("args", True, *(1, "sst", 2)))
+print(function_name("args", True, *{1, "sst", 2}))
+print(function_name("args", True, *{'a': 1, 'b': "sst", 'c': 2}))
+print(function_name("args", False, *{'a': 1, 'b': "sst", 'c': 2}))
+print(function_name("kwargs", False, **{'a': 1, 'b': "sst", 'c': 2}))
